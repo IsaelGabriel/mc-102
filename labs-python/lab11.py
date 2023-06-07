0148018c0f9ccaff2811b0c19b2c35bc8bb3dd71
IsaelGabriel <isaelgabrielfaria@gmail.com>
2023-06-06 22:13:17 -0300

    lab11: updated coordinates to use Vector2

--- labs-python/lab11.py
+++ labs-python/lab11.py
@@ -1,6 +1,6 @@
 class Vector2:
-    def __init__(self, i: int, j: int):
-        self._coordinates: tuple[int, int] = (i, j)
+    def __init__(self, coordinates: tuple[int, int]):
+        self._coordinates: tuple[int, int] = coordinates
     
     @property
     def x(self) -> int:
@@ -23,48 +23,50 @@ class Enemy:
         self._hp: int = int(args[0])
         self._atk: int = int(args[1])
         self._behaviour: str = args[2]
-        self._position: tuple[int] = tuple(map(int, args[3].split(',')))
+        self._position: Vector2 = Vector2(tuple(map(int, args[3].split(','))))
     
     @property
     def position(self) -> tuple[int, int]:
         return self._position
     
     @position.setter
-    def position(self, new_position: tuple[int, int]):
-        self._position = tuple(new_position[0], new_position[1])
+    def position(self, new_position: Vector2):
+        self._position = new_position
 
 class Item:
     def __init__(self, args: list):
         self._name: str = args[0]
         self._behaviour: str = args[1]
-        self._position: tuple[int] = tuple(map(int, args[2].split(',')))
+        self._position: Vector2 = Vector2(tuple(map(int, args[2].split(','))))
         self._value: int = int(args[3])
 
     @property
-    def position(self) -> tuple[int, int]:
+    def position(self) -> Vector2:
         return self._position
     
     @position.setter
-    def position(self, new_position: tuple[int, int]):
-        self._position = tuple(new_position[0], new_position[1])
+    def position(self, new_position: Vector2):
+        self._position = new_position
 
-def input_coordinates(sep: str = " ") -> tuple[int]:
-    return tuple(map(int, input().split(sep)))
+def input_coordinates(sep: str = " ") -> Vector2:
+    return Vector2(tuple(map(int, input().split(sep))))
 
-def formatted_game_map(dimensions: tuple[int], enemies: list[Enemy], items: list[Item]) -> list[list]:
-    game_map = [[[] for columns in range(dimensions[1])] for lines in range(dimensions[0])]
+def formatted_game_map(dimensions: Vector2, enemies: list[Enemy], items: list[Item]) -> list[list]:
+    game_map = [[[] for columns in range(dimensions.x)] for lines in range(dimensions.y)]
 
     for enemy in enemies:
-        game_map[enemy.position[0]][enemy.position[1]].append("E")
+        game_map[enemy.position.y][enemy.position.x].append("E")
     
     for item in items:
-        game_map[item.position[0]][item.position[1]].append("I")
+        game_map[item.position.y][item.position.x].append("I")
+    
+    return game_map
 
 def main():
     initial_hp, initial_dmg = map(int, input().split())
-    map_dimensions: tuple[int] = input_coordinates()
-    inital_position: tuple[int] = input_coordinates(',')
-    exit_position: tuple[int] = input_coordinates(',')
+    map_dimensions: Vector2 = input_coordinates()
+    inital_position: Vector2 = input_coordinates(',')
+    exit_position: Vector2 = input_coordinates(',')
     monster_quantity: int = int(input())
     monsters: list[Enemy] = list(map(Enemy, [input().split() for _ in range(monster_quantity)]))
     item_quantity: int = int(input())
